87b52979ba127015fd61ce68f1b56d22374a4979
Douglas Ribeiro <douglas.ribeiro.id@gmail.com>
2023-12-21 15:27:56 -0300

    First Commit

---
+++ src/main/java/com/douglas/developer/radio/musica/RadioMusicaApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.radio.musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioMusicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douglas/developer/radio/musica/RadioMusicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.developer.radio.musica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioMusicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
